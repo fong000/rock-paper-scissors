765bd859a2a40cc1b70fcadf39eefde779fe0b8b
Fong000 <fongrhyann@gmail.com>
2023-10-11 01:48:31 +0900

    Finish base project

--- javascript.js
+++ javascript.js
@@ -1,28 +1,64 @@
-
-//play a single round of rock paper scissors
-    //computer chooses rock, paper, or scissors
-    const computerSelection = ["Rock", "Paper", "Scissors"];
+//create function for the game
+function game() {
+    //code for game
+    function playRound(playerSelection, computerSelection) {
+        if (computerSelection === "Rock" && playerSelection.toLowerCase() === "rock") {
+            return result = "It's a Tie!";
+        }
+        if (computerSelection === "Rock" && playerSelection.toLowerCase() === "paper") {
+            return result = "You Win! Paper beats Rock!";
+        }
+        if (computerSelection === "Rock" && playerSelection.toLowerCase() === "scissors") {
+            return result = "You Lose! Rock beats Scissors!";
+        }
+        if (computerSelection === "Paper" && playerSelection.toLowerCase() === "paper") {
+            return result = "It's a Tie!";
+        }
+        if (computerSelection === "Paper" && playerSelection.toLowerCase() === "scissors") {
+            return result = "You win! Scissors beats Paper!";
+        }
+        if (computerSelection === "Paper" && playerSelection.toLowerCase() === "rock") {
+            return result = "You Lose! Paper beats Rock!";
+        }
+        if (computerSelection === "Scissors" && playerSelection.toLowerCase() === "scissors") {
+            return result = "It's a Tie!";
+        }
+        if (computerSelection === "Scissors" && playerSelection.toLowerCase() === "rock") {
+            return result = "You win! Rock beats Scissors";
+        }
+        if (computerSelection === "Scissors" && playerSelection.toLowerCase() === "paper") {
+            return result = "You Lose! Scissors beats Paper!";
+        }
+    }
     
-    const random = Math.floor(Math.random() * computerSelection.length);
-    console.log(computerSelection[random]);
+    //displays the match results
+    let result; 
+    //computer memorizes choice 
+    function getComputerChoice() {
+        if (computer[random] === "Rock") {
+            return "Rock";
+        } else if (computer[random] === "Paper") {
+            return "Paper";
+        } else if (computer[random] === "Scissors") {
+            return "Scissors";
+        }
+    }
+    //computer selects randomly
+    const computer = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * computer.length);
+    //displays computer's choice (just for reference)
+    console.log(getComputerChoice());
+    //player chooses
+    const playerSelection = prompt("Choose Rock, Paper, or Scissors.");
+    //changes function to variable
+    const computerSelection = getComputerChoice();
+    //prints results (change later to return, not console.log)    
+    console.log(playRound(playerSelection, computerSelection));
+}
 
-
-//player inputs rock, paper, or scissors (not 'case sensitive)
-//if computer chooses rock and player chooses paper
-    //return You win! Paper beats Rock!
-//else if computer chooses rock and player chooses scissors
-    //return You lose! Rock beats Scissors!
-//else 
-    //return It's a tie!
-//if computer chooses scissors and player chooses rock
-    //return You win! Rock beats scissors!
-//else if computer chooses scissors and player chooses paper
-    //return You lose! Scissors beats Paper!
-//else 
-    //return It's a tie!
-//if computer chooses paper and player chooses scissors
-    //return You win! Scissors beats Paper!
-//else if computer chooses paper and player chooses rock
-    //return You Lose! Paper beats Rock!
-//else
-    //return It's a tie!
+    //make the function loop 5 times
+    game()
+    game()
+    game()
+    game()
+    game()
